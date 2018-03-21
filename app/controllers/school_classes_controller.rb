0e9e87a8ece458495474724886e9c9cb06906440
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-03-21 22:24:49 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,7 +1,7 @@
 class SchoolClassesController < ApplicationController
   def create
     school_class = SchoolClass.create(params.require(:school_class).permit(:title, :room_number))
-    redirect_to school_classes_path(@school_class)
+    redirect_to school_class_path(@school_class)
   end
 
   def show
